974478eaf679b504cfb60907cdfb155b55ac59a8
Ajay K <ajaycoolkarni@yahoo.com>
2021-11-26 02:35:27 +1030

    required tables added

--- db/schema.sql
+++ db/schema.sql
@@ -1,4 +1,36 @@
-DROP DATABASE IF EXISTS companyEmployees_db;
-CREATE DATABASE companyEmployees_db;
+DROP DATABASE IF EXISTS company_employees_db;
+CREATE DATABASE company_employees_db;
 
-USE companyEmployees_db;
+USE company_employees_db;
+
+-- department table
+CREATE TABLE department (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(30) NOT NULL
+);
+
+-- roles table
+CREATE TABLE role (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL,
+  department_id INT,
+  FOREIGN KEY (department_id)
+  REFERENCES department(id)
+  ON DELETE CASCADE
+);
+
+-- employee table - 2 FOREIGN IDs - each employee has ID but they may also have manager ID
+CREATE TABLE employee (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT,
+  FOREIGN KEY (role_id)
+  REFERENCES role(id)
+  ON DELETE SET NULL,
+  manager_id INT,
+  FOREIGN KEY (manager_id)
+  REFERENCES employee(id)
+  ON DELETE SET NULL
+);
