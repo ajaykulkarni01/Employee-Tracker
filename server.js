9e9d5c360eb5b954b2cfafd05958037f8b95df56
Ajay K <ajaycoolkarni@yahoo.com>
2021-11-26 03:46:48 +1030

    a splash screen in text console with logo added

--- server.js
+++ server.js
@@ -1,2 +1,8 @@
 const inquirer = require('inquirer');
 const mysql = require('mysql2');
+
+// a splash screen in text console with logo from ASCII characters.
+const logo = require('asciiart-logo');
+const config = require('./package.json');
+console.log(logo(config).render());
+
