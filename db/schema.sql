16653baf4decdfae71bb8f4cc469d1d2aedfe1c5
Ajay K <ajaycoolkarni@yahoo.com>
2021-11-29 16:59:28 +1030

    modified schema with employee table and added comments

--- db/schema.sql
+++ db/schema.sql
@@ -4,33 +4,27 @@ CREATE DATABASE company_employees_db;
 USE company_employees_db;
 
 -- department table
-CREATE TABLE department (
+CREATE TABLE department(
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(30) NOT NULL
 );
 
 -- roles table
-CREATE TABLE role (
+CREATE TABLE role(
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   title VARCHAR(30) NOT NULL,
   salary DECIMAL,
   department_id INT,
-  FOREIGN KEY (department_id)
-  REFERENCES department(id)
-  ON DELETE CASCADE
+  FOREIGN KEY (department_id)  REFERENCES department(id) ON DELETE CASCADE
 );
 
 -- employee table - 2 FOREIGN IDs - each employee has ID but they may also have manager ID
-CREATE TABLE employee (
+CREATE TABLE employee(
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  first_name VARCHAR(30) NOT NULL,
-  last_name VARCHAR(30) NOT NULL,
+  first_name VARCHAR(30),
+  last_name VARCHAR(30),
   role_id INT,
-  FOREIGN KEY (role_id)
-  REFERENCES role(id)
-  ON DELETE SET NULL,
   manager_id INT,
-  FOREIGN KEY (manager_id)
-  REFERENCES employee(id)
-  ON DELETE SET NULL
+  FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE SET NULL,
+  FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
 );
